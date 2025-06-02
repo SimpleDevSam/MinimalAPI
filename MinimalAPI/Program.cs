fd28b391e4946653cb4ab2cc3a7c7bb960196824
Samuel Maia <samuel.assis.ext@acelen.com>
2025-06-02 17:45:07 -0300

    .

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddValidatorsServices();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
