c353337fdba7e91618fa217647cc39e26b713fb5
Samuel Maia <samuel.assis.ext@acelen.com>
2025-06-06 09:48:20 -0300

    .

--- Tests/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cfb753cc61ce4329875e184719ba25034c703b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029c1a9057f07e20b6032d20493070830bf81b40")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
